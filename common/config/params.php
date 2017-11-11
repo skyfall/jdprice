992cf779042b1fd15e9df27230f24d41acfb278f
sun <sunhongenhotmail.com>
2017-11-11 12:33:00 +0800

    fix

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,10 @@
 <?php
 return [
     'appList'=>[
-        'wx3929189dbf11f00c'=>[
+        'gh_7ab163847a5a'=>[
             'Token'=>'jdprice',
             'EncodingAESKey'=>'80e31a168b49b877d2b9e3ce2c07a99e',
+            'appid'=>'wx3929189dbf11f00c',
         ]
     ],
     //令牌失效时间
