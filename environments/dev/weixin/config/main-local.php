8be8abe49d212621ed1eb0f56d88fabf7f165705
sun <sunhongenhotmail.com>
2017-11-11 12:29:59 +0800

    fix

--- environments/dev/weixin/config/main-local.php
+++ environments/dev/weixin/config/main-local.php
@@ -14,11 +14,15 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs'=>['127.0.0.1', '::1','218.109.233.166']
+
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs'=>['127.0.0.1', '::1','218.109.233.166']
+
     ];
 }
 
